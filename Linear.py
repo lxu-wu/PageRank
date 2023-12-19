f90d07628e19bf7eb1c4f31fbf83ee78db4f7153
guferon <2guillaumeferon@gmail.com>
2023-12-19 10:40:00 +0100

    Correction normalisation matrice + variables names

--- Linear.py
+++ Linear.py
@@ -2,11 +2,26 @@ import numpy as np
 
 
 def PageRankLinear(A,alpha,v):
-    normalized_A_col = A / A.sum(axis=0)
-    pageRank_One = np.identity(A.shape[0])-np.multiply(alpha,normalized_A_col)
+    M_norm = A / A.sum(axis=1)
+    pageRank_One = np.identity(A.shape[0])-np.multiply(alpha,M_norm)
     pageRank_Two = np.multiply((1-alpha),np.linalg.inv(pageRank_One))
-    pageRank_Final = np.matmul(v,pageRank_Two)
-    return pageRank_Final
+    x = np.matmul(v,pageRank_Two)
+    return x / x.sum()
 
 
-print(PageRankLinear(np.matrix([[0,0,1/3,0],[1/2,0,1/3,0],[1/2,0,0,1],[0, 1, 1/3, 0]]),0.85,(0.2,0.5,0.3,0.2)))
+A= np.matrix([[0,0,1],[2,0,1],[0,2,0]])
+
+v= np.array([1,2,3])
+
+print(PageRankLinear(A,0.9,v))
+
+N = 4  # Number of nodes
+A = np.matrix([[0, 1, 1, 0], 
+               [1, 0, 1, 1], 
+               [0, 0, 0, 1], 
+               [0, 0, 1, 0]])  # Example adjacency matrix
+alpha = 0.9  # Teleportation parameter
+v = np.array([1/N] * N)  # Uniform personalization vector
+
+pageRankScores = PageRankLinear(A, alpha, v)
+print(pageRankScores)
