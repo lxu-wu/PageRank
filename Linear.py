27b725bb7da327c4c79453e5312bab49b5bbc71a
guferon <2guillaumeferon@gmail.com>
2023-12-20 13:48:30 +0100

    Ménage file

--- Linear.py
+++ Linear.py
@@ -1,27 +1,20 @@
 import numpy as np
 
 
-def PageRankLinear(A,alpha,v):
+def PageRankLinear(A: np.matrix ,alpha: float ,v:np.array):
     M_norm = A / A.sum(axis=1)
+
     pageRank_One = np.identity(A.shape[0])-np.multiply(alpha,M_norm)
     pageRank_Two = np.multiply((1-alpha),np.linalg.inv(pageRank_One))
-    x = np.matmul(v,pageRank_Two)
+    x = np.matmul(v,np.array(pageRank_Two))
     return x / x.sum()
 
+def PageRankLinearSys(A, alpha, v):
+     transitionProbabilityMatrix = A / A.sum(axis=1)
+     lenght = transitionProbabilityMatrix.shape[0]
 
-A= np.matrix([[0,0,1],[2,0,1],[0,2,0]])
+     A = (np.identity(lenght) - np.multiply(alpha,transitionProbabilityMatrix)).transpose()
+     G = np.linalg.solve(A,v.transpose())
+     G = G/G.sum()
 
-v= np.array([1,2,3])
-
-print(PageRankLinear(A,0.9,v))
-
-N = 4  # Number of nodes
-A = np.matrix([[0, 1, 1, 0], 
-               [1, 0, 1, 1], 
-               [0, 0, 0, 1], 
-               [0, 0, 1, 0]])  # Example adjacency matrix
-alpha = 0.9  # Teleportation parameter
-v = np.array([1/N] * N)  # Uniform personalization vector
-
-pageRankScores = PageRankLinear(A, alpha, v)
-print(pageRankScores)
+     return G
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from Linear import PageRankLinear
-from Power import PageRankPower
+
 
 
 class TestPageRankLinearFunction(unittest.TestCase):
@@ -29,29 +29,6 @@ class TestPageRankLinearFunction(unittest.TestCase):
         expected_result = np.array([0.03757225, 0.04190751, 0.46470946, 0.45581077])
         np.testing.assert_array_almost_equal(result, expected_result)
 
-    def test_pageRankPower_1(self):
-        # Test avec des valeurs par défaut (alpha=0.9, pas de vecteur de personnalisation)
-        A= np.matrix([[0,0,1],[2,0,1],[0,2,0]])
-        alpha = 0.9
-        v= np.array([1,2,3])
-        
-        result = PageRankPower(A,alpha,v)
-        expected_result = np.array([0.24248634, 0.37636612, 0.38114754])
-        np.testing.assert_array_almost_equal(result, expected_result)
-    
-    def test_PageRankPower_2(self):
-        N = 4  # Number of nodes
-        A = np.matrix([[0, 1, 1, 0], 
-               [1, 0, 1, 1], 
-               [0, 0, 0, 1], 
-               [0, 0, 1, 0]]) 
-        alpha = 0.9 
-        v = np.array([1/N] * N)  # Uniform personalization vector
-        
-        result = PageRankLinear(A,alpha,v)
-        expected_result = np.array([0.03757225, 0.04190751, 0.46470946, 0.45581077])
-        np.testing.assert_array_almost_equal(result, expected_result)
-    
 
 
 if __name__ == '__main__':
