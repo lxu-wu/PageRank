211a99b0549b0457932e4cc467a1678c47aa6dd1
guferon <2guillaumeferon@gmail.com>
2023-12-23 15:08:46 +0100

    add test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from Linear import PageRankLinear
-
+from Power import pageRankPower
 
 
 class TestPageRankLinearFunction(unittest.TestCase):
@@ -28,6 +28,30 @@ class TestPageRankLinearFunction(unittest.TestCase):
         result = PageRankLinear(A,alpha,v)
         expected_result = np.array([0.03757225, 0.04190751, 0.46470946, 0.45581077])
         np.testing.assert_array_almost_equal(result, expected_result)
+
+    def test_pageRankPower_1(self):
+        # Test avec des valeurs par défaut (alpha=0.9, pas de vecteur de personnalisation)
+        A= np.matrix([[0,0,1],[2,0,1],[0,2,0]])
+        alpha = 0.9
+        v= np.array([1,2,3])
+        
+        result = PageRankLinear(A,alpha,v)
+        expected_result = np.array([0.24248634, 0.37636612, 0.38114754])
+        np.testing.assert_array_almost_equal(result, expected_result)
+
+    def test_pageRankPower_1(self):
+        # Test avec des valeurs par défaut (alpha=0.9, pas de vecteur de personnalisation)
+        A= np.matrix([[0,0,1],[2,0,1],[0,2,0]])
+        alpha = 0.9
+        v= np.array([1,2,3])
+        
+        result = pageRankPower(A,alpha,v)
+        expected_result = np.array([0.24248634, 0.37636612, 0.38114754])
+        np.testing.assert_array_almost_equal(result, expected_result)
+
+
+
+
 
 
 
